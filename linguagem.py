31292101ac423b6cd51093839fc5e588335f5e11
lucian-canto-lopes <147873300+lucian-canto-lopes@users.noreply.github.com>
2024-05-25 23:38:57 -0300

    Update and rename tut to linguagem.py

---
+++ linguagem.py
@@ -0,0 +1,9 @@
+class Aluno:
+  def __init__(self,aluno,turma):
+    self.aluno = aluno
+    self.turma = turma
+
+a1 = Aluno("lucian","BCC 23")
+
+print("aluno:" + a1.aluno, "\nturma:" + a1.turma)
+  
